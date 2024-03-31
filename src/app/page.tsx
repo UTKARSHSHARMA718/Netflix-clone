be84de63833a8b0dfdca464ca6a293092a25aa57
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2024-03-31 15:27:47 +0530

    feat: added billboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Billboard from "@/components/Billboard/Billboard";
 import Navbar from "@/containers/Navbar/Navbar";
+import { getMoviesAndSeries } from '@/actions/getMoviesAndSeries';
 
-export default function Home() {
+export default async function Home() {
+  const allMoviesAndSeries = await getMoviesAndSeries({
+    genre:"comed",
+  });
+
   return (
     <>
       <Navbar />
+      <Billboard {...{ allMoviesAndSeries }} />
     </>
   )
 }
